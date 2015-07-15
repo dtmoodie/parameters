02e21ff0f34fb4708ca68115720cfcd2909d289c
dtmoodie <dtmoodie@gmail.com>
2015-07-15 08:12:41 -0400

    asdf

---
+++ src/UI/Ui.cpp
@@ -0,0 +1,15 @@
+#include "UI/UI.hpp"
+
+using namespace Parameters::UI;
+
+boost::asio::io_service UiCallbackService::service;
+boost::asio::io_service ProcessingThreadCallbackService::service;
+
+boost::asio::io_service& ProcessingThreadCallbackService::Instance()
+{
+	return service;
+}
+boost::asio::io_service& UiCallbackService::Instance()
+{
+	return service;
+}
