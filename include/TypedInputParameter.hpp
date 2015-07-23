8f9a43556fd72448a869f3a9d19e650670f901d7
Dan <dtmoodie>
2015-07-23 14:33:07 -0400

    Added another input parameter type.

--- include/TypedInputParameter.hpp
+++ include/TypedInputParameter.hpp
@@ -85,6 +85,85 @@ namespace Parameters
 
 	};
 
+	template<typename T> class TypedInputParameterCopy : public MetaTypedParameter<T>, public InputParameter
+	{
+		T* userVar; // Pointer to the user space variable of type T
+		typename ITypedParameter<T>::Ptr input;
+		boost::signals2::connection inputConnection;
+		virtual void onInputUpdate()
+		{
+			// The input variable has been updated, update user var
+			*userVar = *input->Data();
+			Parameter::changed = true;
+			Parameter::UpdateSignal();
+		}
+	public:
+		typedef std::shared_ptr<TypedInputParameterCopy<T>> Ptr;
+		static Ptr create(T* userVar_)
+		{
+			return Ptr(new TypedInputParameterCopy(userVar_));
+		}
+		TypedInputParameterCopy(const std::string& name, T* userVar_,
+			const Parameter::ParameterType& type = Parameter::ParameterType::Control, const std::string& tooltip = "") :
+			MetaTypedParameter<T>(name, type, tooltip), userVar(userVar_) {}
+
+		virtual bool SetInput(const std::string& name_)
+		{
+			return false;
+		}
+
+		virtual bool SetInput(const Parameter::Ptr param)
+		{
+			typename ITypedParameter<T>::Ptr castedParam = std::dynamic_pointer_cast<ITypedParameter<T>>(param);
+			if (castedParam)
+			{
+				input = castedParam;
+				inputConnection.disconnect();
+				inputConnection = castedParam->RegisterNotifier(boost::bind(&TypedInputParameterCopy<T>::onInputUpdate, this));
+				*userVar = *input->Data();
+				return true;
+			}
+			return false;
+		}
+		std::shared_ptr<Parameter> GetInput()
+		{
+			return input;
+		}
+
+		virtual bool AcceptsInput(const Parameter::Ptr param)
+		{
+			if (qualifier)
+				return qualifier(param.get());
+			return Loki::TypeInfo(typeid(T)) == param->GetTypeInfo();
+		}
+
+		virtual bool AcceptsType(const Loki::TypeInfo& type)
+		{
+			return Loki::TypeInfo(typeid(T)) == type;
+		}
+		virtual T* Data()
+		{
+			return userVar;
+		}
+		virtual void UpdateData(T& data_)
+		{
+			*userVar = data_;
+		}
+		virtual void UpdateData(const T& data_)
+		{
+			*userVar = data_;
+		}
+		virtual void UpdateData(T* data_)
+		{
+			*userVar = *data_;
+		}
+		virtual Loki::TypeInfo GetTypeInfo()
+		{
+			return Loki::TypeInfo(typeid(T));
+		}
+
+	};
+
 	// Meant to reference a pointer variable in user space, and to update that variable whenever 
 	// IE int* myVar; 
 	// auto typedParam = TypedInputParameterPtr(&myVar); // TypedInputParameter now updates myvar to point to whatever the
