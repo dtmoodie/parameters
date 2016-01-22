bdf0de564fdfb2b04a3b30e2edbb12dc28ae94ab
Dan Moodie <dtmoodie@gmail.com>
2016-01-22 13:26:11 -0500

    Added license.

--- include/UI/Qt.hpp
+++ include/UI/Qt.hpp
@@ -1,4 +1,23 @@
+/*
+Copyright (c) 2015 Daniel Moodie.
+All rights reserved.
+
+Redistribution and use in source and binary forms are permitted
+provided that the above copyright notice and this paragraph are
+duplicated in all such forms and that any documentation,
+advertising materials, and other materials related to such
+distribution and use acknowledge that the software was developed
+by the Daniel Moodie. The name of
+Daniel Moodie may not be used to endorse or promote products derived
+from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+https://github.com/dtmoodie/parameters
+*/
 #pragma once
+#ifdef Qt5_FOUND
 #include "../Parameter_def.hpp"
 #include <QtWidgets/QWidget>
 #include <type_traits>
@@ -1060,3 +1079,4 @@ namespace Parameters
 	} /* namespace UI */
 } /* namespace Perameters */
 
+#endif // Qt5_FOUND
--- include/UI/UI.hpp
+++ include/UI/UI.hpp
@@ -1,3 +1,21 @@
+/*
+Copyright (c) 2015 Daniel Moodie.
+All rights reserved.
+
+Redistribution and use in source and binary forms are permitted
+provided that the above copyright notice and this paragraph are
+duplicated in all such forms and that any documentation,
+advertising materials, and other materials related to such
+distribution and use acknowledge that the software was developed
+by the Daniel Moodie. The name of
+Daniel Moodie may not be used to endorse or promote products derived
+from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+https://github.com/dtmoodie/parameters
+*/
 #pragma once
 
 #ifdef PARAMETERS_GENERATE_UI
@@ -7,10 +25,13 @@ namespace Parameters
 {
 	namespace UI
 	{
-		template<typename T> class UiPolicy: public qt::QtUiPolicy<T>
+		template<typename T> class UiPolicy //: public qt::QtUiPolicy<T>
 		{
 		public:
-
+            UiPolicy()
+            {
+                static qt::QtUiPolicy<T> qt_policy;
+            }
 		};
 	}
 }
